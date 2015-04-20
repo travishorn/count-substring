24d7f978c15ac06d7e557881b03198c9184e3996
Travis Horn <horn.travis@gmail.com>
2015-04-20 15:53:34 -0500

    Test for counting commas.

--- test.js
+++ test.js
@@ -20,9 +20,13 @@ describe('count-substring', function() {
 
   it('Works with slashes in substring', function() {
     countSubstring('foo bar', '/bar/').should.equal(0);
-  })
+  });
 
   it('Works with slashes in string and substring', function() {
     countSubstring('foo /bar/ bar', '/bar/').should.equal(1);
-  })
+  });
+
+  it('Counts commas', function() {
+    countSubstring('Cats, dogs, and fish.', ',').should.equal(2);
+  });
 });
